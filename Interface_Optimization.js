421b4a7e947a5721dc593597eaf537415c899f5e
cssxsh <cssxsh@gmail.com>
2019-01-16 00:02:07 +0800

    filter_

--- Interface_Optimization.js
+++ Interface_Optimization.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Interface Optimization
 // @namespace    https://github.com/cssxsh/Guet_SctCoz_Plug-ins
-// @version      0.2.3
+// @version      0.2.5
 // @description  对选课系统做一些优化
 // @author       cssxsh
 // @include      http://bkjw.guet.edu.cn/Login/MainDesktop
