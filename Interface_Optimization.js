34407f68fc2058769d51e8fcdcfe39ed19fd3cbe
cssxsh <cssxsh@gmail.com>
2019-02-28 22:43:26 +0800

    getTime

--- Interface_Optimization.js
+++ Interface_Optimization.js
@@ -30,6 +30,7 @@ Ext.onReady(function () {
 				qryfrm.items.items.forEach(function (item) { item.editable = true; });
 				//重写Grid
 				var ctb = Ext.create("Edu.view.coursetable");
+
 				var newGrid = Ext.create("Ext.grid.Panel", {
 					columnLines: true,
 					width: "100%", height: "100%", minHeight: 400, layout: "fit",
@@ -92,6 +93,20 @@ Ext.onReady(function () {
 						title: "课程表", width: "80%", height:"80%", modal: true, resizable: false, layout: "fit",
 						items: [panView]
 					}).show();
+					if (ctb.store.data.length == 0) {
+						ctb.store.proxy.url = "https://github.com/cssxsh/Guet_SctCoz_Plug-ins/raw/master/Json/GetHourInfo.json";
+						ctb.store.load();
+					}
+					if (ctb.store.data.length == 0) {
+						ctb.store.loadData([
+							{"term": "2018-2019_2", "nodeno": "1", "nodename": "<font size=1>上午第一节</font></br>", "memo": "08:25-10:00", "xq1": "", "xq2": "", "xq3": "", "xq4": "", "xq5": "", "xq5": "", "xq6": "", "xq7": ""},
+							{"term": "2018-2019_2", "nodeno": "2", "nodename": "<font size=1>上午第二节</font></br>", "memo": "10:25-12:00", "xq1": "", "xq2": "", "xq3": "", "xq4": "", "xq5": "", "xq5": "", "xq6": "", "xq7": ""},
+							{"term": "2018-2019_2", "nodeno": "3", "nodename": "<font size=1>下午第一节</font></br>", "memo": "14:30-16:05", "xq1": "", "xq2": "", "xq3": "", "xq4": "", "xq5": "", "xq5": "", "xq6": "", "xq7": ""},
+							{"term": "2018-2019_2", "nodeno": "4", "nodename": "<font size=1>下午第二节</font></br>", "memo": "16:30-18:05", "xq1": "", "xq2": "", "xq3": "", "xq4": "", "xq5": "", "xq5": "", "xq6": "", "xq7": ""},
+							{"term": "2018-2019_2", "nodeno": "5", "nodename": "<font size=1>晚上第一节</font></br>", "memo": "19:00-20:35", "xq1": "", "xq2": "", "xq3": "", "xq4": "", "xq5": "", "xq5": "", "xq6": "", "xq7": ""},
+							{"term": "2018-2019_2", "nodeno": "6", "nodename": "<font size=1>晚上第二节</font></br>", "memo": "21:00-22:35", "xq1": "", "xq2": "", "xq3": "", "xq4": "", "xq5": "", "xq5": "", "xq6": "", "xq7": ""},
+						], true);
+					}
 					ctb.render(panView.body, gRec);
 				}
 				function queryStore() {
