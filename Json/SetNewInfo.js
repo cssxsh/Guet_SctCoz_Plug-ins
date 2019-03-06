16b3c3a2564050a4aecb42eeca4aead1456506d2
cssxsh <cssxsh@gmail.com>
2019-03-06 19:18:44 +0800

    NewInfoJsApi.

--- Json/SetNewInfo.js
+++ Json/SetNewInfo.js
@@ -1,5 +1,21 @@
 window.plugTools.ClassStorage.Set("value", "NewInfo", function (data) {
     console.log("test");
-    var result =  "test";
+    var result =  { 
+        "id": "info-1",
+        "title": "这是一个测试",
+        "content": "test", 
+        "postdate": null,
+        "operator": "插件",
+        "ntype": null,
+        "reader": function (me) {
+            Ext.create("Ext.window.Window", {
+                title: this.title, width: "40%", height:"40%", modal: true, resizable: true, layout: "fit",
+                items: [{xtype: "form", autoScroll: true, frame: true, padding: "1", html: me.content.replace(/\n/g, "<br/>")}]
+            }).show();
+        },
+        "showdate": "2019年03月06日",
+        "chk": null,
+        "openshow": 0
+        };
     return result;
 });
