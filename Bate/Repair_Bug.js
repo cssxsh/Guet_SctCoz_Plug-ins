41ab9fc74448b019612a15d5cc411d47c48f6986
cssxsh <cssxsh@gmail.com>
2019-03-24 14:10:12 +0800

    StuDelayNew

--- Bate/Repair_Bug.js
+++ Bate/Repair_Bug.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Repair Bug
 // @namespace    https://github.com/cssxsh/Guet_SctCoz_Plug-ins
-// @version      1.1.1
+// @version      1.1.4
 // @description  修补选课系统的bug
 // @author       cssxsh
 // @include      http://bkjw.guet.edu.cn/Login/MainDesktop
@@ -116,7 +116,139 @@ Ext.onReady(function () {
             }
         }
     }
+    let StuDelayNew = {
+        action: "StuDelay",
+        text: "缓考申请",
+        id: "StuDelay",
+        listeners: {
+            afterrender: function (me, opt) {
+                let tmSto = Ext.data.StoreManager.lookup("xqSto");
+                let dlSto = Ext.create("Ext.data.Store", {
+                    fields: ["term", "grade", "spno", "courseid", "courseno", "cname", "stid", "name", "stype", "zt", "lb", "dptno", "reason"],
+                    proxy: {
+                        type: "ajax", 
+                        url: "/student/getstudelay", 
+                        api: { 
+                            create: "/student/adddelay",
+                            update: "/student/chgdelay", 
+                            destroy: "/student/deldelay" 
+                        },
+                        reader: { 
+                            type: "json", 
+                            root: "data" 
+                        }
+                    }
+                });
+                let cnoSto = Ext.create("Ext.data.Store", {
+                    fields: ["id", "term", "courseno", "stid", "cname", "xf", "name", "tname", "xm", "xksj", "courseid", "stype", "ip", "xf", { name: "comm", defaultValue: "1@1" }],
+                    proxy: {
+                        type: "ajax", 
+                        url: "/student/GetSctCourse",
+                        reader: { 
+                            type: "json", 
+                            root: "data" 
+                        }
+                    }
+                });
+                let ztAry = ["未提交", "已提交", "审核通过", "不通过"];
+                var qryfrm = Ext.create("Edu.view.QueryForm", {
+                    url: "/student/stuinfo",
+                    labelWidth: 60, 
+                    region: "north", 
+                    argcols: [
+                        { xtype: "hidden", name: "dptno" }, 
+                        { xtype: "hidden", name: "lb", value: 1 }, 
+                        { xtype: "termcombo", name: "term", store: tmSto, allowBlank: false, labelWidth: 35 }, 
+                        { xtype: "button", text: "查询", formBind: true, margin: "0 3", handler: queryStore }
+                    ]
+                });
+                qryfrm.load();
+                var dlGd = Ext.create("Edu.view.ShowGrid", {
+                    region: "center",
+                    width: "50%", 
+                    plugins: [ Ext.create("Ext.grid.plugin.CellEditing", { clicksToEdit: 1 }) ],
+                    store: dlSto,
+                    columns: [
+                        { xtype: "actionrendercolumn", header: "操作", width: 70, renderer: function (v, m, r) { return (r.data.zt == 0) ? ["提交", "删除"] : [];}, 
+                            items: [{
+                                handler: function (grid, rowIndex, colIndex) {
+                                    let sto = grid.getStore();
+                                    let rec = sto.getAt(rowIndex);
+                                    if (rec.get("zt") == 0) {
+                                        let ly = rec.get("reason");
+                                        if (!ly || ly.trim().length < 4) return Ext.Msg.alert("提示", "必须输入缓考理由");
+                                        rec.set("zt",1); 
+                                        sto.sync();
+                                        return;
+                                    }
+                                }
+                            }, {
+                                handler: function (grid, rowIndex, colIndex) {
+                                    let sto = grid.getStore();
+                                    let rec = sto.getAt(rowIndex);
+                                    if (rec.get("zt") == 0) {
+                                        sto.remove(rec); 
+                                        sto.sync();
+                                        return;
+                                    }
+                                }
+                            }]
+                        },
+                        { header: "姓名", dataIndex: "name", width: 60 },
+                        { header: "课号", dataIndex: "courseno", width: 65 },
+                        { header: "课程名称", dataIndex: "cname", width: 120 },
+                        { header: "状态", dataIndex: "zt", width: 50, renderer: function (v) { if (!v) v = 0; if (v >= 0 && v < 4) return ztAry[v]; } },
+                        { header: "申请缓考理由", dataIndex: "reason", minWidth: 180, flex: 1, editor: {} }
+                    ]
+                });
+                var grid = Ext.create("Edu.view.ShowGrid", {
+                    region: "east", 
+                    width: "50%",
+                    store: cnoSto,
+                    tbar:[{ text: "添加", handler: addXk }],
+                    columns: [
+                        { header: "序号", xtype: "rownumberer", width: 30 },
+                        { header: "课号", dataIndex: "courseno", width: 65 },
+                        { header: "课程名称", dataIndex: "cname",width: 180 },
+                        { header: "课程性质", dataIndex: "tname", width: 70 }
+                    ]
+                });
+
+                function queryStore() {
+                    let args = qryfrm.getForm().getValues();
+                    cnoSto.proxy.extraParams = args;
+                    dlSto.proxy.extraParams = args;
+                    cnoSto.load();
+                    dlSto.load();
+                }
+                function addXk() {
+                    let rs = grid.getSelectionModel().getSelection();
+                    if (rs.length < 1) Ext.Msg.alert("请选择学生");
+                    let qm = qryfrm.getValues();
+                    rs.forEach(function (m) {
+                        let nr = new dlSto.model();
+                        Ext.apply(nr.data, m.data, qm);
+                        m.set("zt", 0);
+                        dlSto.add(nr);
+                    });
+                    dlSto.sync();
+                }
+
+                let pan = Ext.create("Edu.view.ShowPanel", {
+                    items: [ qryfrm, dlGd, grid ]
+                });
+                me.add(pan);
+                plugTools.Logger(me.items.items.length, 2, "sss");
+
+                // 隐藏多余的旧模块
+                me.addListener("add", function (me, newPan, opt) {
+                    newPan.hide();
+                });
+            }
+        }
+    }
     plugTools.menuChange(StuPersonNew);
     plugTools.menuChange(StuLabNew);
     plugTools.menuChange(StuLvlNew);
+    plugTools.menuChange(StuDelayNew);
 });
