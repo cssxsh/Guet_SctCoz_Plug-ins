bbcf3d1658d35b2fb189f8297f7f88491b21f3de
cssxsh <cssxsh@gmail.com>
2019-04-18 09:56:06 +0800

    search

--- Bate/Interface_Optimization.js
+++ Bate/Interface_Optimization.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Interface Optimization
 // @namespace    https://github.com/cssxsh/Guet_SctCoz_Plug-ins
-// @version      3.7.2
+// @version      3.7.3
 // @description  对选课系统做一些优化
 // @author       cssxsh
 // @include      http://bkjw.guet.edu.cn/Login/MainDesktop
