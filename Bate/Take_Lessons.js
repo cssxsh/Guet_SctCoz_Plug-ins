c88f1bccb58a4c154f0da6da515fb5fd2d1a25bb
cssxsh <cssxsh@gmail.com>
2019-09-21 21:00:59 +0800

    require tools.js

--- Bate/Take_Lessons.js
+++ Bate/Take_Lessons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Take Lessons
 // @namespace    https://github.com/cssxsh/Guet_SctCoz_Plug-ins
-// @version      4.7.7
+// @version      4.7.8
 // @description  新教务抢课脚本
 // @author       cssxsh
 // @include      http://bkjw.guet.edu.cn/Login/MainDesktop
